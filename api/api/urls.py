cf189dff2a1d71bab0c03df03c1410301cba3b88
Kirill Kirillov <kirusfg@gmail.com>
2022-05-27 11:39:58 +0500

    Add a /api/v1 URI prefix

--- api/api/urls.py
+++ api/api/urls.py
@@ -23,7 +23,7 @@ router = routers.DefaultRouter()
 router.register(r'users', users_views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/',
          include('rest_framework.urls', namespace='rest_framework'))
