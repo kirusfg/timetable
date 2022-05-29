eb6d30a3bd8678ce6a80f9a3a00f6d3353a5db98
Kirill Kirillov <kirusfg@gmail.com>
2022-05-29 20:11:06 +0500

    Add a convenient backend API access point

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,8 +1,23 @@
-import { create } from "apisauce";
+import { ApiResponse, create } from "apisauce";
+import User from "./models/User";
 
 const api = create({
-    baseURL: 'https://localhost:8000/api/v1',
+    baseURL: process.env.API_URI,
     timeout: 10000,
-})
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
-export default api
+export const fetcher = (uri: string) => api.get(uri)
+    .then((res: ApiResponse<any>): User[] => {
+        const { ok, data } = res;
+
+        if (!ok) {
+            console.error(ok);
+        }
+
+        return data.results;
+    });
+
+export default api;
