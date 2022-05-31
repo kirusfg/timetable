aac9efed39df36a43b4a2931af56984dd04fa399
Kirill Kirillov <kirusfg@gmail.com>
2022-05-31 19:00:34 +0500

    Rename User into Student

--- api/apps/users/models.py
+++ api/apps/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from apps.timetable.models import AcademicLevel
 
 
-class User(User):
+class Student(User):
     year_of_study = models.IntegerField()
     academic_level = models.ForeignKey(
         AcademicLevel, on_delete=models.DO_NOTHING)
