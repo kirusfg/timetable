7378a958b73891c5b96b68769e0599bb6faf3c45
Kirill Kirillov <kirusfg@gmail.com>
2022-06-09 15:04:51 +0500

    Create a basic page layout (to be moved to a separate component)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import Stack from '@mui/material/Stack';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+
 const Home = () => {
-  return (<p>Home</p>);
+  return (
+    <>
+    <Box sx={{
+      width: '100%',
+      bgcolor: 'background.level2',
+    }}>
+      <Stack>
+        <Box sx={{
+          bgcolor: 'background.body',
+          padding: 4,
+        }}>
+          <Typography level='h2'>Home</Typography>
+        </Box>
+        <Box sx={{
+          padding: 4,
+        }}>
+          <Typography level='body1'>Page body</Typography>
+        </Box>
+      </Stack>
+    </Box>
+    </>
+  );
 }
 
 export default Home;
