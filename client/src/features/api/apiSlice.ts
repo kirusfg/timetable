8e67e4f47f6065d59bfdc8db6813063a54cc9e54
Kirill Kirillov <kirusfg@gmail.com>
2022-06-16 11:57:22 +0500

    Set the return type for one of the RTK Query endpoints

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { Course } from '../../types/Course';
+
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1' }),
   endpoints: builder => ({
-    getCourses: builder.query({ query: () => '/courses/' }),
+    getCourses: builder.query<Course[], void>({ query: () => '/courses/' }),
     getSections: builder.query({ query: () => '/sections/' }),
     getUsers: builder.query({ query: () => '/users/' }),
   }),
---
+++ client/src/types/Course.ts
@@ -0,0 +1,10 @@
+export type Course = {
+  id: number;
+  instance: number;
+  abbr: string;
+  title: string;
+  desc: string;
+  credits_ects: number;
+  credits_us: number;
+  department: string;
+}
