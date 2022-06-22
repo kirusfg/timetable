036ca58dbb43a92ed2a38c6f2001ff490ba56029
Kirill Kirillov <kirusfg@gmail.com>
2022-06-22 08:52:18 +0500

    Make the Section draggable

--- client/src/components/course/Section.tsx
+++ client/src/components/course/Section.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { useDrag } from "react-dnd";
+
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
+// import { ItemTypes } from "../../types/DnD";
 import Section from "../../types/Section";
 
 
@@ -14,8 +17,15 @@ interface SectionCardProps {
 const SectionCard: React.FC<SectionCardProps> = (props) => {
   let { section } = props;
 
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "section",
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    })
+  }));
+
   return (
-    <Card sx={{ maxWidth: 200 }}>
+    <Card ref={drag} sx={{ maxWidth: 200, opacity: isDragging ? 0.5 : 1 }}>
       <CardContent>
         <Typography variant="body2">
           {section.number + " " + section.type}
