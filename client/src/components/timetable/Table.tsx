e8ac7b1aaee098033b261489529ab90c88c88e71
Kirill Kirillov <kirusfg@gmail.com>
2022-06-22 09:17:27 +0500

    Make the Table droppable

--- client/src/components/timetable/Table.tsx
+++ client/src/components/timetable/Table.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 
-import { useDrag } from "react-dnd";
+import { useDrop } from "react-dnd";
 
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 
-// import { ItemTypes } from "../../types/DnD";
 import Section from "../../types/Section";
 
 
 interface TableProps {
+  days?: number;
   sections?: Section[];
 }
 
 const Table: React.FC<TableProps> = (props) => {
   let { sections } = props;
 
-  // const [{ isDragging }, drag] = useDrag(() => ({
-  //   type: "section",
-  //   collect: (monitor) => ({
-  //     isDragging: !!monitor.isDragging(),
-  //   })
-  // }));
+  const [, drop] = useDrop(
+    () => ({
+      accept: "section",
+      drop: () => console.log("Section added")
+    }),
+  );
 
   return (
-    <Box sx={{ width: "100%", height: "100%" }}>
+    <Box ref={drop} sx={{ width: "100%", height: "100%" }}>
       <Paper>
         Hello
       </Paper>
