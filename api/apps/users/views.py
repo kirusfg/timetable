3308f3faa1f7336e6373d0f3724d14c11968390b
Kirill Kirillov <kirusfg@gmail.com>
2022-06-28 17:15:54 +0500

    Make each Student be "fetcheable" for their saved schedules

--- api/apps/users/views.py
+++ api/apps/users/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from apps.timetable.models.schedule import Schedule
 from .models import Student
 from .serializers import StudentSerializer
 
@@ -8,3 +12,14 @@ class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
     permission_classes = [permissions.AllowAny]
     # lookup_field = 'username'
+
+    @action(detail=True)
+    def schedules(self, request, pk=None):
+        student = self.get_object()
+        schedules = [s for s in Schedule.objects.all() if s.student == student]
+        return Response({
+            'count': len(schedules),
+            'next': 'null',
+            'previous': 'null',
+            'results': schedules,
+        })
