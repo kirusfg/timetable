92f46f2753a1caf2a057c05b873671c15fcd9c2c
Kirill Kirillov <kirusfg@gmail.com>
2022-07-04 18:09:12 +0500

    Commit ongoing work before switching to Vite

--- client/src/pages/Timetable.tsx
+++ client/src/pages/Timetable.tsx
@@ -2,7 +2,7 @@ import Stack from "@mui/material/Stack";
 
 import Page from "./Page";
 
-import Selector from "../components/timetable/Selector";
+import Selector from "../components/timetable/selector/Selector";
 import Table from "../components/timetable/table/Table";
 
 
---
+++ client/src/types/SectionGroup.ts
@@ -0,0 +1,10 @@
+import Section from "./Section";
+
+type SectionGroup = {
+    course: number;
+    instance: number;
+    "type": string;
+    sections: Section[];
+};
+
+export default SectionGroup;
