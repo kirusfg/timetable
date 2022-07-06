cf66e760020cdd20b8411eea4aeceb9480941c19
Kirill Kirillov <kirusfg@gmail.com>
2022-07-06 20:31:24 +0500

    Format the file

--- client/src/app/store/api/apiSlice.ts
+++ client/src/app/store/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Course from "../../../types/Course";
 import Section from "../../../types/Section";
 import User from "../../../types/User";
-import {groupSections} from "../timetable/timetableSlice";
+import { groupSections } from "../timetable/timetableSlice";
 
 export type ApiResponse<T> = {
   count: number;
@@ -21,7 +21,7 @@ export const apiSlice = createApi({
     }),
     getSections: builder.query<ApiResponse<Section>, void>({
       query: () => "/sections/",
-      async onQueryStarted(arg, {dispatch, queryFulfilled}) {
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
           const { data: sections } = await queryFulfilled
           dispatch(groupSections(sections.results))
