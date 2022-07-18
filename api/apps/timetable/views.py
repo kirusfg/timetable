710d0e76c06d6075c5c0ef446d5f71e284f133fe
Kirill Kirillov <kirusfg@gmail.com>
2022-07-18 09:50:05 +0500

    Make the timetable routes available only for the authenticated users

--- api/apps/timetable/views.py
+++ api/apps/timetable/views.py
@@ -15,16 +15,16 @@ from .models.section import Section
 class SemesterViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Semester.objects.all()
     serializer_class = SemesterSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class SectionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
